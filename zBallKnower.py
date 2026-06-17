cced91a6731ec3f797e0e3ec35ae2e1caf7a6526
Kenneth Tyburczy <ktyburczy23@gmail.com>
2026-06-17 16:55:53 -0500

    New data and some logic

--- zBallKnower.py
+++ zBallKnower.py
@@ -1,15 +1,41 @@
 import csv
 import random
 
-with open("CleanedPlayers.csv", newline="") as f:
-    reader = csv.DictReader(f)
-    row = random.choice(list(reader))
+def randomPlayer():
+    with open("Data/CleanedPlayers.csv", newline="") as f:
+        reader = csv.DictReader(f)
+        row = random.choice(list(reader))
 
-player_id = row["id"]
-last      = row["last"]
-first     = row["first"]
-team      = row["team"]
-position  = row["position"]
-season    = row["season"]
+    player_id = row["id"]
+    last      = row["last"]
+    first     = row["first"]
+    team      = row["team"]
+    position  = row["position"]
+    season    = row["season"]
 
-print(f"{first} {last} played for {team} as {position} in {season}")
+    
+    print(f" This player played for {team} as {position} in {season}")
+    answer = input('Input your guess: ')
+    if answer == last + ' ' + first:
+        print('Congrats! You got it right!')
+    else:
+        print('Sorry thats not correct. The correct answer is ' + first + ' ' + last)
+
+def chooseEra():
+    print('era')
+
+def chooseTeam():
+    print('team')
+
+if __name__ == "__main__":
+
+    print('1. Guess the Player\n2. Chose an Era\n3. Chose a Team\n4. Exit')
+    frontpageOptions = input('Select option: ')
+    if frontpageOptions == '1':
+        randomPlayer()
+    elif frontpageOptions == '2':
+        chooseEra()
+    elif frontpageOptions == '3':
+        chooseTeam()
+    else:
+        exit
